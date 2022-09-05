8f268ee0c0c39782c9bd1e502832f24844aa2ffd
monkhaus <tom.monkhouse95@gmail.com>
2022-09-05 10:56:31 +0100

    add project structure script

---
+++ create-api-project-structure.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+mkdir api
+cd api
+
+mkdir models
+mkdir serializers
+mkdir tests
+mkdir views
+mkdir migrations
+mkdir permissions
+
+touch models/__init__.py
+touch serializers/__init__.py
+touch tests/__init__.py
+touch views/__init__.py
+touch migrations/__init__.py
+touch permissions/__init__.py
+touch urls.py
+touch admin.py
